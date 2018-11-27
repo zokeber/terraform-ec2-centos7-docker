58fbe5ebbc3ade11045dab1044d45078dde29cf0
Daniel Lopez Monagas <zokeber@gmail.com>
2018-11-27 15:28:55 -0300

    Add output for elastic ip

--- outputs.tf
+++ outputs.tf
@@ -38,6 +38,10 @@ output "ec2_public_ip" {
   value = "${aws_instance.ec2.public_ip}"
 }
 
+output "elastic_ip" {
+  value = "${aws_eip.elastic_ip.*.public_ip}"
+}
+
 output "user" {
   value = "${var.user_connection}"
 }
