22ff8b2edb94d1d9911bd8f329338d34b9ea4465
Daniel Lopez Monagas <zokeber@gmail.com>
2018-11-28 19:02:24 -0300

    Add change path for docker-compose

--- script.sh
+++ script.sh
@@ -6,8 +6,8 @@ sudo yum remove docker docker-common docker-selinux docker-engine -y
 sudo yum install vim epel-release yum-utils device-mapper-persistent-data lvm2 -y
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo -y
 sudo yum install docker-ce -y
-sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.22.0/docker-compose-$(uname -s)-$(uname -m)" -o /bin/docker-compose
+sudo chmod +x /bin/docker-compose
 sudo systemctl enable docker.service
 sudo systemctl start docker.service
 echo "alias docker='sudo docker'" >> ~/.bashrc
